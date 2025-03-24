07f69682890352eac1b72b867743c7346639e5a0
Dennis L <114615839+luongkhuong@users.noreply.github.com>
2025-03-24 15:57:19 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Login from './components/Login.jsx'
+import About from './components/About.jsx'
+import Navbar from './components/Navbar.jsx'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Market from './components/Market.jsx';
+import Portfolio from './components/Portfolio.jsx';
 
 function App() {
 
+  const [user, setUser] = useState({
+    user: '',
+    isLoggedIn: false
+  });
 
-  return (
-    <>
-     <h1>welcome</h1>
-     
-    </>
-  )
+  const handleLogout = () =>{
+    setUser((prevState) => ({
+      ...prevState,
+      isLoggedIn: false
+    }));
+  }
+
+ return (
+ <>
+ <Router>
+  {user.isLoggedIn && <Navbar  handleLogout = {handleLogout} />}
+  <Routes>
+    <Route path= "/" element={user.isLoggedIn ? <Portfolio /> : <Login setUserInParentComponent = {setUser} />} /> {/*set Home as Portfolio for now*/}
+    <Route path= "/about" element={user.isLoggedIn ? <About /> : <Login setUserInParentComponent = {setUser} />} />
+    <Route path= "/market" element={user.isLoggedIn ? <Market /> : <Login setUserInParentComponent = {setUser} />} />
+    <Route path= "/portfolio" element={user.isLoggedIn ? <Portfolio /> : <Login setUserInParentComponent = {setUser} />} />
+  </Routes>
+  </Router>
+  
+ </ >
+
+
+);
 }
 
 export default App
